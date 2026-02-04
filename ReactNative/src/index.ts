f0bce22853154f94cd0ec56d714c4ce3a7749b6b
Gabe Roeloffs <gdroel@gmail.com>
2026-02-03 22:55:55 -0800

    remove react native

--- ReactNative/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @zerosettle/react-native
- *
- * React Native bindings for ZeroSettle iOS SDK components.
- */
-
-export { ZSMigrateTipView } from './ZSMigrateTipView';
-export type { ZSMigrateTipViewProps } from './ZSMigrateTipView';
